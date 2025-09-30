df9e32cce137cd0bc7fd91a17375190b67372f2b
Glauco Todesco <glauco.todesco@gmail.com>
2025-09-30 14:32:46 -0300

    Initial Commit

---
+++ src/test/java/com/fatec/horario/HorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.horario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
