7fe9ed35c46fe579116a41e8be68fb8392194c71
Millena S. Zovadelli <millenaszovadelli@gmail.com>
2025-11-13 16:22:11 -0300

    ✨ feat: Entidade UserAvailability (Models, DTOs e Mappers)

---
+++ src/main/java/com/fatec/horario/entities/UserAvailability.java
@@ -0,0 +1,88 @@
+package com.fatec.horario.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "user availability")
+public class UserAvailability {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int weekday;
+    private int lessonNumber;
+
+
+    public UserAvailability(Long id, int weekday, int lessonNumber) {
+        this.id = id;
+        this.weekday = weekday;
+        this.lessonNumber = lessonNumber;
+    }
+
+
+    public UserAvailability() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public int getWeekday() {
+        return weekday;
+    }
+
+
+    public void setWeekday(int weekday) {
+        this.weekday = weekday;
+    }
+
+
+    public int getLessonNumber() {
+        return lessonNumber;
+    }
+
+
+    public void setLessonNumber(int lessonNumber) {
+        this.lessonNumber = lessonNumber;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserAvailability other = (UserAvailability) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
