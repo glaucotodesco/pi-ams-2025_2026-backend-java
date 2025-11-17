09f805e28106338ed59326cfbef29c8252fff2d6
Gttolla <lucas12chves@gmail.com>
2025-11-17 16:29:47 -0300

    :recycle: refactor: chenged @Min and @Max validations to @Positve

--- src/main/java/com/fatec/horario/dtos/ScheduleRequest.java
+++ src/main/java/com/fatec/horario/dtos/ScheduleRequest.java
@@ -1,20 +1,15 @@
 package com.fatec.horario.dtos;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record ScheduleRequest(
 
     @NotNull(message = "Lesson Number is required")
-    @Min(value = 1, message = "Lesson Number must be greater than or equal to 1")
-    @Max(value = 6, message = "Lesson Number must be less than or equal to 6")
+    @Positive(message = "Lesson Number must be positive")
     Integer lessonNumber,
 
-    @NotBlank(message = "Weekday is required")
-    @Min(value = 1, message = "Weekday must be greater than or equal to 1")
-    @Max(value = 7, message = "Weekday must be less than or equal to 7")
+    @NotNull(message = "Weekday is required")
+    @Positive(message = "Weekday must be positive")
     Integer weekday
-
 ) {}
