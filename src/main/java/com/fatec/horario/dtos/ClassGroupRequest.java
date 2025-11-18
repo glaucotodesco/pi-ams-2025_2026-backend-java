63e0436d5df4e4cd8f813778f49ed5532b8991df
Gttolla <lucas12chves@gmail.com>
2025-11-18 16:27:56 -0300

    :recycle: refactor: changed ClassGroupRequest validations

--- src/main/java/com/fatec/horario/dtos/ClassGroupRequest.java
+++ src/main/java/com/fatec/horario/dtos/ClassGroupRequest.java
@@ -1,15 +1,13 @@
 package com.fatec.horario.dtos;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record ClassGroupRequest(
 
- @NotBlank
- @Min(value = 1, message = "is required, This field must be filled in.")
- Integer studentCount
-
+    @NotNull(message = "Student Count is required") 
+    @Positive(message = "Student Count must positive") 
+    Integer studentCount
 ) {
 
-    
 }
