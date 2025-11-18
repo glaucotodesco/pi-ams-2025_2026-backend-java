817ff907a79d2466bbab5bf554eb55b02d37158d
Tigaas <trr08469@gmail.com>
2025-11-18 14:47:35 -0300

    :construction: feat: start Course service layer with CourseRepository

--- src/main/java/com/fatec/horario/repositories/CourseRepository.java
+++ src/main/java/com/fatec/horario/repositories/CourseRepository.java
@@ -1,5 +1,11 @@
 package com.fatec.horario.repositories;
 
-public class CourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fatec.horario.entities.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
     
 }
