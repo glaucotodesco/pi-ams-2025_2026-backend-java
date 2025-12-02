2b98fe6d42d1cb3e44a93f2d1ed92d59ab12f605
Gttolla <lucas12chves@gmail.com>
2025-12-02 17:33:41 -0300

    :recycle: refactor: changed ScheduleController methods namo to improve standardization

--- src/main/java/com/fatec/horario/controllers/ScheduleController.java
+++ src/main/java/com/fatec/horario/controllers/ScheduleController.java
@@ -1,16 +1,26 @@
 package com.fatec.horario.controllers;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.fatec.horario.dtos.ScheduleRequest;
 import com.fatec.horario.dtos.ScheduleResponse;
 import com.fatec.horario.services.ScheduleService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/schedules")
@@ -21,19 +31,19 @@ public class ScheduleController {
     private ScheduleService service;
 
     @GetMapping
-    public ResponseEntity<List<ScheduleResponse>> getAllSchedules() {
+    public ResponseEntity<List<ScheduleResponse>> getAll() {
         List<ScheduleResponse> list = service.getAll();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ScheduleResponse> getScheduleById(@PathVariable Long id) {
+    public ResponseEntity<ScheduleResponse> getById(@PathVariable Long id) {
         ScheduleResponse response = service.getById(id);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping
-    public ResponseEntity<ScheduleResponse> createSchedule(
+    public ResponseEntity<ScheduleResponse> create(
             @Valid @RequestBody ScheduleRequest request) {
 
         ScheduleResponse response = service.create(request);
@@ -48,7 +58,7 @@ public class ScheduleController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ScheduleResponse> updateSchedule(
+    public ResponseEntity<ScheduleResponse> update(
             @PathVariable Long id,
             @Valid @RequestBody ScheduleRequest request) {
         ScheduleResponse response = service.update(id, request);
@@ -56,7 +66,7 @@ public class ScheduleController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSchedule(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
